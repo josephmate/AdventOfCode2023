52e0a3ef60ab9017b727a1990e078fd61773f148
Joseph Mate <mate.joseph@gmail.com>
2023-12-01 12:16:28 -0500

    almost done. just need to add all the cases

--- day01.go
+++ day01.go
@@ -18,16 +18,55 @@ func calcNumber(str string) int {
 	return firstLast[0]*10 + firstLast[len(firstLast)-1]
 }
 
+func reverseString(s string) []byte {
+	var rev []byte
+
+	// Reversing the slice of runes
+	for i := len(s) - 1; i >= 0; i++ {
+		rev = append(rev, s[i])
+	}
+
+	// Converting the reversed slice of runes back to a string
+	return rev
+}
+
+func getNumFromBuffer(substring []byte) (bool, int) {
+	if substring[0] == 'z' &&
+		substring[1] == 'e' &&
+		substring[2] == 'r' &&
+		substring[3] == 'o' {
+		return true, 0
+	} else if substring[0] == 'o' &&
+		substring[1] == 'n' &&
+		substring[2] == 'e' {
+		return true, 1
+	}
+	return false, 0
+}
+
+func calcNumberFromWords(str string) int {
+	var firstLast []int
+	for i := 0; i < len(str); i++ {
+		substring := reverseString(str[0:i])
+		hasNumber, value := getNumFromBuffer(substring)
+		if hasNumber {
+			firstLast = append(firstLast, value)
+		}
+	}
+	return firstLast[0]*10 + firstLast[len(firstLast)-1]
+}
+
 func Day1() {
 
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: aoc 1 <inputfile>")
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: aoc 1 <part 1 input> <part 2 input>")
 		os.Exit(1)
 	}
 
-	filename := os.Args[2]
+	filenamePart1 := os.Args[2]
+	filenamePart2 := os.Args[3]
 	// Open the file
-	file, err := os.Open(filename)
+	file, err := os.Open(filenamePart1)
 	if err != nil {
 		log.Fatalf("Error opening file: %s", err)
 	}
@@ -41,14 +80,30 @@ func Day1() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		sum += calcNumber(line)
-		calcNumber(line)
 	}
-
+	// Check for any scanner errors
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading file: %s", err)
+	}
+	file.Close()
 	fmt.Println("Part 1:")
 	fmt.Println(sum)
 
+	part2File, err2 := os.Open(filenamePart2)
+	if err != nil {
+		log.Fatalf("Error opening file: %s", err2)
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scannerPart2 := bufio.NewScanner(part2File)
+	var part2Sum = 0
+	for scannerPart2.Scan() {
+		line := scannerPart2.Text()
+		part2Sum += calcNumberFromWords(line)
+	}
 	// Check for any scanner errors
-	if err := scanner.Err(); err != nil {
+	if err := scannerPart2.Err(); err != nil {
 		log.Fatalf("Error reading file: %s", err)
 	}
 }
