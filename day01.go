2056c585ab5037045c42026d77693cc926381d76
Joseph Mate <mate.joseph@gmail.com>
2023-12-01 11:57:33 -0500

    part1 complete

--- day01.go
+++ day01.go
@@ -7,6 +7,17 @@ import (
 	"os"
 )
 
+func calcNumber(str string) int {
+	var firstLast []int
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		if char >= '0' && char <= '9' {
+			firstLast = append(firstLast, int(char-'0'))
+		}
+	}
+	return firstLast[0]*10 + firstLast[len(firstLast)-1]
+}
+
 func Day1() {
 
 	if len(os.Args) < 3 {
@@ -25,11 +36,16 @@ func Day1() {
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 
+	var sum = 0
 	// Read line by line
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line) // Process each line as needed
+		sum += calcNumber(line)
+		calcNumber(line)
 	}
+
+	fmt.Println("Part 1:")
+	fmt.Println(sum)
 
 	// Check for any scanner errors
 	if err := scanner.Err(); err != nil {
