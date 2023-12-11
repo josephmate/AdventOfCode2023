9a5045763ff49a45457e68786d3ffccf0a522448
Joseph Mate <mate.joseph@gmail.com>
2023-12-11 06:42:25 -0500

    day10 part 1

--- day10.go
+++ day10.go
@@ -46,7 +46,7 @@ func expandMoves(maze []string, r int, c int) [][3]int {
 		moves = append(moves, [3]int{r - 1, c, NORTH})
 	}
 	if r+1 < len(maze) {
-		moves = append(moves, [3]int{r - 1, c, NORTH})
+		moves = append(moves, [3]int{r + 1, c, SOUTH})
 	}
 	if c-1 >= 0 {
 		moves = append(moves, [3]int{r, c - 1, WEST})
@@ -58,6 +58,17 @@ func expandMoves(maze []string, r int, c int) [][3]int {
 	return moves
 }
 
+/*
+| is a vertical pipe connecting north and south.
+- is a horizontal pipe connecting east and west.
+L is a 90-degree bend connecting north and east.
+J is a 90-degree bend connecting north and west.
+7 is a 90-degree bend connecting south and west.
+F is a 90-degree bend connecting south and east.
+. is ground; there is no pipe in this tile.
+S is the starting position
+*/
+
 func findFurthest(maze []string) int {
 	startR, startC := findStart(maze)
 	if DEBUG {
@@ -65,7 +76,7 @@ func findFurthest(maze []string) int {
 	}
 
 	var longestSoFar = 0
-	var visited map[[2]int]bool
+	visited := map[[2]int]bool{}
 	visited[[2]int{startR, startC}] = true
 	var queue [][4]int
 	for _, startingMove := range expandMoves(maze, startR, startC) {
@@ -73,6 +84,9 @@ func findFurthest(maze []string) int {
 	}
 
 	for len(queue) > 0 {
+		if DEBUG {
+			fmt.Println("findFurthest", "longestSoFar", longestSoFar, "queue", queue)
+		}
 		nextEntry := queue[0]
 		queue = queue[1:]
 		r := nextEntry[0]
@@ -83,6 +97,131 @@ func findFurthest(maze []string) int {
 		if visited[[2]int{r, c}] {
 			continue
 		}
+
+		pipe := maze[r][c]
+		/*
+		   | is a vertical pipe connecting north and south.
+		   - is a horizontal pipe connecting east and west.
+		   L is a 90-degree bend connecting north and east.
+		   J is a 90-degree bend connecting north and west.
+		   7 is a 90-degree bend connecting south and west.
+		   F is a 90-degree bend connecting south and east.
+		   . is ground; there is no pipe in this tile.
+		   S is the starting position
+		*/
+		if pipe == '.' || pipe == 'S' {
+			continue
+		} else if pipe == '|' && direction == NORTH {
+			// | is a vertical pipe connecting north and south.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r-1 >= 0 {
+				queue = append(queue, [4]int{r - 1, c, NORTH, distance + 1})
+			}
+		} else if pipe == '|' && direction == SOUTH {
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r+1 < len(maze) {
+				queue = append(queue, [4]int{r + 1, c, SOUTH, distance + 1})
+			}
+		} else if pipe == '-' && direction == EAST {
+			// - is a horizontal pipe connecting east and west.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c+1 < len(maze[r]) {
+				queue = append(queue, [4]int{r, c + 1, EAST, distance + 1})
+			}
+		} else if pipe == '-' && direction == WEST {
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c-1 >= 0 {
+				queue = append(queue, [4]int{r, c - 1, WEST, distance + 1})
+			}
+		} else if pipe == 'L' && direction == SOUTH {
+			// L is a 90-degree bend connecting north and east.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c+1 < len(maze[r]) {
+				queue = append(queue, [4]int{r, c + 1, EAST, distance + 1})
+			}
+		} else if pipe == 'L' && direction == WEST {
+			// L is a 90-degree bend connecting north and east.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r-1 >= 0 {
+				queue = append(queue, [4]int{r - 1, c, NORTH, distance + 1})
+			}
+		} else if pipe == 'J' && direction == EAST {
+			// J is a 90-degree bend connecting north and west.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r-1 >= 0 {
+				queue = append(queue, [4]int{r - 1, c, NORTH, distance + 1})
+			}
+		} else if pipe == 'J' && direction == SOUTH {
+			// J is a 90-degree bend connecting north and west.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c-1 >= 0 {
+				queue = append(queue, [4]int{r, c - 1, WEST, distance + 1})
+			}
+		} else if pipe == '7' && direction == NORTH {
+			// 7 is a 90-degree bend connecting south and west.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c-1 >= 0 {
+				queue = append(queue, [4]int{r, c - 1, WEST, distance + 1})
+			}
+		} else if pipe == '7' && direction == EAST {
+			// 7 is a 90-degree bend connecting south and west.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r+1 < len(maze) {
+				queue = append(queue, [4]int{r + 1, c, SOUTH, distance + 1})
+			}
+		} else if pipe == 'F' && direction == NORTH {
+			// F is a 90-degree bend connecting south and east.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if c+1 < len(maze[r]) {
+				queue = append(queue, [4]int{r, c + 1, EAST, distance + 1})
+			}
+		} else if pipe == 'F' && direction == WEST {
+			// F is a 90-degree bend connecting south and east.
+			if distance > longestSoFar {
+				longestSoFar = distance
+			}
+			visited[[2]int{r, c}] = true
+			if r+1 < len(maze) {
+				queue = append(queue, [4]int{r + 1, c, SOUTH, distance + 1})
+			}
+		} else {
+			//fmt.Println("findFurthest unexpected pipe ", string(pipe), " at r=", r, "c=", c)
+			//return 0
+		}
+
 	}
 
 	return longestSoFar
