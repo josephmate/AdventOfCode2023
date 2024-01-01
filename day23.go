24c1e61094b43654ed4281244d4110209c3f3b91
Joseph Mate <mate.joseph@gmail.com>
2024-01-01 09:53:39 -0500

    tried using a hash that does not depend on order for deduplicating paths but wasn't efficient enough
    trying to use a bloom filter instead of using a set to track the path taken, but now i'm not getting a solution

--- day23.go
+++ day23.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"os"
+
+	"github.com/bits-and-blooms/bloom/v3"
 )
 
 type HikingPath struct {
 	CurrentPosition [2]int
 	Visited         map[[2]int]bool
+	HashOfPath      uint
+	VisitedBloom    *bloom.BloomFilter
 }
 
 func cloneHikingPath(hikingPath HikingPath) HikingPath {
@@ -37,6 +42,7 @@ func printHikingPath(hikingMap [][]byte, longestSoFarPath HikingPath) {
 		fmt.Println()
 	}
 }
+
 
 func findLongestPath(hikingMap [][]byte) int {
 	startPosn := [2]int{0, 1}
@@ -145,6 +151,163 @@ func findLongestPath(hikingMap [][]byte) int {
 	return longestSoFar[endPosn] - 1
 }
 
+/*
+https://stackoverflow.com/questions/28326965/good-hash-function-for-list-of-integers-where-order-doesnt-change-value
+*/
+func hash(hikingMap [][]byte, posn [2]int) uint {
+	var x = uint(posn[0]*len(hikingMap[0]) + posn[1])
+	x ^= x >> 17;
+	x *= uint(0xed5ad4bb);
+	x ^= x >> 11;
+	x *= uint(0xac4c1b51);
+	x ^= x >> 15;
+	x *= uint(0x31848bab);
+	x ^= x >> 14;
+	return x
+}
+
+func addToBloom(hikingMap [][]byte, hikingPath HikingPath, posn [2]int) {
+	i := uint32(posn[0] * len(hikingMap[0]) + posn[1])
+	n1 := make([]byte, 4)
+	binary.BigEndian.PutUint32(n1, i)
+	hikingPath.VisitedBloom.Add(n1)
+}
+
+func containsBloom(hikingMap [][]byte, hikingPath HikingPath, posn [2]int) bool {
+	i := uint32(posn[0] * len(hikingMap[0]) + posn[1])
+	n1 := make([]byte, 4)
+	binary.BigEndian.PutUint32(n1, i)
+	return hikingPath.VisitedBloom.Test(n1)
+}
+
+func findLongestPathIgnoreSlopes(hikingMap [][]byte) int {
+	startPosn := [2]int{0, 1}
+	numRows := len(hikingMap)
+	numCols := len(hikingMap[0])
+	endPosn := [2]int{numRows - 1, numCols - 2}
+	var queue []HikingPath
+	startPath := HikingPath{}
+	startPath.CurrentPosition = startPosn
+	startPath.VisitedBloom = bloom.NewWithEstimates(uint(len(hikingMap))*uint(len(hikingMap[0])), 0.0001)
+	m, k := bloom.EstimateParameters(uint(len(hikingMap))*uint(len(hikingMap[0])), 0.0001)
+	addToBloom(hikingMap, startPath, startPosn)
+	fmt.Println("estimate params m=", m, "k=", k)
+	startPath.HashOfPath = hash(hikingMap, startPosn)
+	queue = append(queue, startPath)
+
+	longestSoFar := map[[2]int]int{}
+	longestSoFarPath := map[[2]int]HikingPath{}
+	// try to deduplicate paths using the hash of the path so far
+	dedupePaths := map[uint]bool{}
+
+	for len(queue) > 0 {
+		currentPath := queue[0]
+		queue = queue[1:]
+
+		if DEBUG {
+			fmt.Println(currentPath)
+		}
+
+		// we already have a longer path thru this point
+		// if longestSoFar[currentPath.CurrentPosition] >= len(currentPath.Visited) {
+		// 	continue
+		// }
+		// longestSoFar[currentPath.CurrentPosition] = len(currentPath.Visited)
+		// longestSoFarPath[currentPath.CurrentPosition] = currentPath
+		if len(currentPath.Visited) > longestSoFar[currentPath.CurrentPosition] {
+			longestSoFar[currentPath.CurrentPosition] = len(currentPath.Visited)
+			longestSoFarPath[currentPath.CurrentPosition] = currentPath
+		}
+
+		dedupePaths[currentPath.HashOfPath] = true
+
+		// out of bounds
+		if currentPath.CurrentPosition[0] < 0 ||
+			currentPath.CurrentPosition[0] >= len(hikingMap) ||
+			currentPath.CurrentPosition[1] < 0 ||
+			currentPath.CurrentPosition[1] >= len(hikingMap[0]) {
+			continue
+		}
+
+		currentTerrain := hikingMap[currentPath.CurrentPosition[0]][currentPath.CurrentPosition[1]]
+		if currentTerrain == '#' {
+			continue // cannot travel thru the forest
+		}
+
+		var nextPosn = [2]int{currentPath.CurrentPosition[0], currentPath.CurrentPosition[1] + 1}
+		var nextHash = currentPath.HashOfPath + hash(hikingMap, nextPosn)
+		if !containsBloom(hikingMap, currentPath, nextPosn) &&
+			!dedupePaths[nextHash] &&
+			nextPosn[0] >= 0 &&
+			nextPosn[0] < len(hikingMap) &&
+			nextPosn[1] >= 0 &&
+			nextPosn[1] < len(hikingMap[0]) &&
+			hikingMap[nextPosn[0]][nextPosn[1]] != '#' {
+			nextPath := HikingPath{}
+			nextPath.VisitedBloom = currentPath.VisitedBloom.Copy()
+			addToBloom(hikingMap, nextPath, nextPosn)
+			nextPath.CurrentPosition = nextPosn
+			nextPath.HashOfPath = nextHash
+			queue = append(queue, nextPath)
+		}
+
+		nextPosn = [2]int{currentPath.CurrentPosition[0] + 1, currentPath.CurrentPosition[1]}
+		nextHash = currentPath.HashOfPath + hash(hikingMap, nextPosn)
+		if !containsBloom(hikingMap, currentPath, nextPosn) &&
+			!dedupePaths[nextHash] &&
+			nextPosn[0] >= 0 &&
+			nextPosn[0] < len(hikingMap) &&
+			nextPosn[1] >= 0 &&
+			nextPosn[1] < len(hikingMap[0]) &&
+			hikingMap[nextPosn[0]][nextPosn[1]] != '#' {
+			nextPath := HikingPath{}
+			nextPath.VisitedBloom = currentPath.VisitedBloom.Copy()
+			addToBloom(hikingMap, nextPath, nextPosn)
+			nextPath.CurrentPosition = nextPosn
+			nextPath.HashOfPath = nextHash
+			queue = append(queue, nextPath)
+		}
+
+		nextPosn = [2]int{currentPath.CurrentPosition[0], currentPath.CurrentPosition[1] - 1}
+		nextHash = currentPath.HashOfPath + hash(hikingMap, nextPosn)
+		if !containsBloom(hikingMap, currentPath, nextPosn) &&
+			!dedupePaths[nextHash] &&
+			nextPosn[0] >= 0 &&
+			nextPosn[0] < len(hikingMap) &&
+			nextPosn[1] >= 0 &&
+			nextPosn[1] < len(hikingMap[0]) &&
+			hikingMap[nextPosn[0]][nextPosn[1]] != '#' {
+			nextPath := HikingPath{}
+			nextPath.VisitedBloom = currentPath.VisitedBloom.Copy()
+			addToBloom(hikingMap, nextPath, nextPosn)
+			nextPath.CurrentPosition = nextPosn
+			nextPath.HashOfPath = nextHash
+			queue = append(queue, nextPath)
+		}
+
+		nextPosn = [2]int{currentPath.CurrentPosition[0] - 1, currentPath.CurrentPosition[1]}
+		nextHash = currentPath.HashOfPath + hash(hikingMap, nextPosn)
+		if !containsBloom(hikingMap, currentPath, nextPosn) &&
+			!dedupePaths[nextHash] &&
+			nextPosn[0] >= 0 &&
+			nextPosn[0] < len(hikingMap) &&
+			nextPosn[1] >= 0 &&
+			nextPosn[1] < len(hikingMap[0]) &&
+			hikingMap[nextPosn[0]][nextPosn[1]] != '#' {
+			nextPath := HikingPath{}
+			nextPath.VisitedBloom = currentPath.VisitedBloom.Copy()
+			addToBloom(hikingMap, nextPath, nextPosn)
+			nextPath.CurrentPosition = nextPosn
+			nextPath.HashOfPath = nextHash
+			queue = append(queue, nextPath)
+		}
+	}
+
+	printHikingPath(hikingMap, longestSoFarPath[endPosn])
+
+	return longestSoFar[endPosn] - 1
+}
+
 func Day23() {
 
 	if len(os.Args) < 3 {
@@ -165,4 +328,5 @@ func Day23() {
 	fmt.Println(findLongestPath(hikingMap))
 
 	fmt.Println("Part 2:")
+	fmt.Println(findLongestPathIgnoreSlopes(hikingMap))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/josephmate/AdventOfCode2023
 go 1.21.3
 
 require (
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/bits-and-blooms/bloom/v3 v3.6.0 // indirect
 	github.com/davidkleiven/gononlin v0.2.5 // indirect
 	gonum.org/v1/exp v0.0.0-20220503065741-e45430a0816d // indirect
 	gonum.org/v1/gonum v0.14.0 // indirect
